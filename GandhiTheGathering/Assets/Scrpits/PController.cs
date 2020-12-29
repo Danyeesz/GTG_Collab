747f28e6d4685025f98a6ed0bcd85fa43ad3dab5
Danyeesz <daniel12094@gmail.com>
2020-12-29 22:56:23 +0100

    Adding the Default movement

---
+++ GandhiTheGathering/Assets/Scrpits/PController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PController : MonoBehaviour
+{
+    public Camera camP;
+    public NavMeshAgent agetP;
+    public GameObject followers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = camP.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agetP.SetDestination(hit.point);
+            }
+            
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Enemy")
+        {
+            GameObject follow = Instantiate(followers,gameObject.transform.position, gameObject.transform.rotation);
+        }
+    }
+    
+}
