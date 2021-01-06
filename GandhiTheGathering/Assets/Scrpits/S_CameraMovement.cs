62068dbbfb5127c55cad16d34e9d15a690dc025c
Liza Kolpacheva <laquaeprompa@gmail.com>
2021-01-06 20:42:18 +0100

    Added static camera movement with Sway

---
+++ GandhiTheGathering/Assets/Scrpits/S_CameraMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_CameraMovement : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothspeed = 0.125f;
+    public Vector3 offset;
+
+    private void Start()
+    {
+        transform.position = target.transform.position + offset;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredposition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredposition, smoothspeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
