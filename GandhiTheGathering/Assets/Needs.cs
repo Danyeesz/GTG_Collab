c9d8dba736b711cd7505d0d0e81ec8cf5ea15123
Danyeesz <daniel12094@gmail.com>
2021-01-19 18:47:56 +0100

    Needs

---
+++ GandhiTheGathering/Assets/Needs.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Needs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    public Slider FoodVsEqu;
+    public Slider HinduVsReli;
+    public Slider BritainVsIndia;
+
+    private void Start()
+    {
+        FoodVsEqu.value = 0.5f;
+        HinduVsReli.value = 0.5f;
+        BritainVsIndia.value = 0.5f;
+    }
+    // Start is called before the first frame update
+    public void SetNeeds(float need)
+    {
+        FoodVsEqu.value = need;
+        HinduVsReli.value = need;
+    }
+
+    public void SetMaxInsp(float insp)
+    {
+
+        HinduVsReli.maxValue = insp;
+        HinduVsReli.value = insp;
+
+    }
+}
